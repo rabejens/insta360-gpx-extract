a7d64ba648af1d4e3077814b0dff63b493313262
Jens Rabe <rabe-jens@outlook.com>
2022-05-27 16:26:40 +0200

    Fixed the build scripts

--- build.bat
+++ build.bat
@@ -9,14 +9,15 @@ if not exist build\osx-x64 md build\osx-x64
 if not exist build\osx-arm64 md build\osx-arm64
 
 dotnet publish -c Release Insta360GPXExtract
-robocopy /mir Insta360GPXExtract\bin\Release\net6.0\publish\ build\fw\
 dotnet publish -c Release -r win-x86 --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true Insta360GPXExtract
-copy Insta360GPXExtract\bin\Release\net6.0\win-x86\publish\*.exe build\win-x86
 dotnet publish -c Release -r win-x64 --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true Insta360GPXExtract
-copy Insta360GPXExtract\bin\Release\net6.0\win-x64\publish\*.exe build\win-x64
 dotnet publish -c Release -r linux-x64 --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true Insta360GPXExtract
-copy Insta360GPXExtract\bin\Release\net6.0\linux-x64\publish\Insta360GPXExtract build\linux-x64
 dotnet publish -c Release -r osx-x64 --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true Insta360GPXExtract
-copy Insta360GPXExtract\bin\Release\net6.0\osx-x64\publish\Insta360GPXExtract build\osx-x64
 dotnet publish -c Release -r osx-arm64 --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true Insta360GPXExtract
+
+robocopy /mir Insta360GPXExtract\bin\Release\net6.0\publish\ build\fw\
+copy Insta360GPXExtract\bin\Release\net6.0\win-x86\publish\*.exe build\win-x86
+copy Insta360GPXExtract\bin\Release\net6.0\win-x64\publish\*.exe build\win-x64
+copy Insta360GPXExtract\bin\Release\net6.0\linux-x64\publish\Insta360GPXExtract build\linux-x64
+copy Insta360GPXExtract\bin\Release\net6.0\osx-x64\publish\Insta360GPXExtract build\osx-x64
 copy Insta360GPXExtract\bin\Release\net6.0\osx-arm64\publish\Insta360GPXExtract build\osx-arm64
--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ for n in win-x86 win-x64 linux-x64 osx-x64 osx-arm64; do
 	dotnet publish -c Release -r $n --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true Insta360GPXExtract
 	mkdir -p build/$n
 	if [ -f Insta360GPXExtract/bin/Release/net6.0/$n/publish/Insta360GPXExtract.exe ]; then
-		cp Insta360GPXExtract/bin/Release/net6.0/$n/publish/Insta360GPXExtract.exe build/$n
+		cp -f Insta360GPXExtract/bin/Release/net6.0/$n/publish/Insta360GPXExtract.exe build/$n
 	else
-		Insta360GPXExtract/bin/Release/net6.0/$n/publish/Insta360GPXExtract build/$n
+		cp -f Insta360GPXExtract/bin/Release/net6.0/$n/publish/Insta360GPXExtract build/$n
 	fi
 done
